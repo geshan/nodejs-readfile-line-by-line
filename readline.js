9dc5b5bcb0d6d16cb9e0faf5ef3ecad1b3486d64
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-10-07 21:14:34 +1100

    Read file line by line with readline used 6.33 MB memory took 7.3 seconds

---
+++ readline.js
@@ -0,0 +1,24 @@
+const events = require('events');
+const fs = require('fs');
+const readline = require('readline');
+
+(async function processLineByLine() {
+  try {
+    const rl = readline.createInterface({
+      input: fs.createReadStream('broadband.sql'),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+      console.log(`Line from file: ${line}`);
+    });
+
+    await events.once(rl, 'close');
+
+    console.log('Reading file line by line with readline done.');
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
